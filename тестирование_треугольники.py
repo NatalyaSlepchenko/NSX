a18c12e7fb980aba793da4101f245da876cbb7be
NatalyaSlepchenko <121039844+NatalyaSlepchenko@users.noreply.github.com>
2023-10-27 00:49:59 +0300

    тестирование_треугольники.py

--- тестирование_треугольники.py
+++ тестирование_треугольники.py
@@ -7,13 +7,13 @@ def check_triangle():
     z = int(entry3.get())
 
     if x + y <= z or (x == 0 and y == 0 and z == 0) or (x == 1 and y == 1 and z == 1):
-        result_label.config(text="Такого треугольника не существует")
+        result_label.config(text="Треугольника с данными значениями не существует, попробуйте другие значения.")
     elif x > 1 and y > 1 and z > 1 and x == y and y == z and x + y > z:
-        result_label.config(text="Равносторонний треугольник")
+        result_label.config(text="Данный треугольник - равносторонний.")
     elif x > 1 and y > 1 and z > 1 and x + y > z:
-        result_label.config(text="Разносторонний треугольник")
+        result_label.config(text="Данный треугольник - разносторонний.")
     elif (x > 1 and y > 1 and z > 1) and (x == y and x != z or x == z and x != y or y == z and y != x):
-        result_label.config(text="Равнобедренный треугольник")
+        result_label.config(text="Данный треугольник - равнобедренный.")
     else:
         result_label.config(text="Треугольника с данными значениями не существует, попробуйте другие значения.")
 def is_valid(newval):
@@ -51,6 +51,13 @@ check_button = Button(window, text="Результат", fg="Indigo", bg="#FFFF0
 check_button.pack()
 lab1 = Label(window, fg="#D8BFD8", bg="#D8BFD8")
 lab1.pack()
+
+result_label = Label(window, text="", font="Times 15", fg="DarkSlateBlue", bg="#D8BFD8")
+result_label.pack()
+
+
+window.mainloop()
+
 
 result_label = Label(window, text="", font="Times 15", fg="DarkSlateBlue", bg="#D8BFD8")
 result_label.pack()
