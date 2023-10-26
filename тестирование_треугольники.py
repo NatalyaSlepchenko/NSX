7358b91b9dfc5aeebff0c631bf0ca54dbe8cbec4
NatalyaSlepchenko <121039844+NatalyaSlepchenko@users.noreply.github.com>
2023-10-26 22:19:25 +0300

    Add files via upload
    Практическая номер 1. Тестирование.

---
+++ тестирование_треугольники.py
@@ -0,0 +1,61 @@
+from tkinter import*
+import re
+
+def check_triangle():
+    x = int(entry1.get())
+    y = int(entry2.get())
+    z = int(entry3.get())
+
+    if x != 0 and y != 0 and z !=0:
+        result_label.config(text="Треугольника c данными значениями не существует, попробуйте другие значения.")
+        if x == y == z:
+            result_label.config(text="Данный треугольник - равносторонний")
+        if x < y + z or y < x + z or z < x + y:
+            result_label.config(text="Треугольника с данными значениями не существует, попробуйте другие значения.")
+        if x == y or y == z or x == z:
+            result_label.config(text="Данный треугольник - равнобедренный")
+        else:
+            result_label.config(text="Данный треугольник - разносторонний")
+    else:
+        result_label.config(text="Треугольника с данными значениями не существует, попробуйте другие значения.")
+def is_valid(newval):
+    return re.match("^\d{0,4}$", newval) is not None
+
+
+window = Tk()
+window.title("treygolnik")
+window.geometry('550x340')
+window['bg'] = '#D8BFD8'
+
+check = (window.register(is_valid), "%P")
+
+label = Label(window, text="Введите длины сторон треугольника", font="Times 18", bg='#D8BFD8', fg="Indigo")
+label.pack()
+label = Label(window, text="p.s числа должны содержать не больше 4 знаков:)", font="Times 15", bg='#D8BFD8', fg="Indigo")
+label.pack()
+lab1 = Label(window, fg="#E6E6FA", bg="#D8BFD8")
+lab1.pack()
+
+entry1 = Entry(window, font="Times 15", fg="SlateBlue", bg="Lavender", validate="key", validatecommand=check)
+entry1.pack()
+lab1 = Label(window, fg="#D8BFD8", bg="#D8BFD8")
+lab1.pack()
+entry2 = Entry(window, font="Times 15", fg="SlateBlue", bg="Lavender", validate="key", validatecommand=check)
+entry2.pack()
+lab1 = Label(window, fg="#D8BFD8", bg="#D8BFD8")
+lab1.pack()
+entry3 = Entry(window, font="Times 15", fg="SlateBlue", bg="Lavender", validate="key", validatecommand=check)
+entry3.pack()
+lab1 = Label(window, fg="#D8BFD8", bg="#D8BFD8")
+lab1.pack()
+
+check_button = Button(window, text="Результат", fg="Indigo", bg="#FFFF00", width=30, height=2, command=check_triangle)
+check_button.pack()
+lab1 = Label(window, fg="#D8BFD8", bg="#D8BFD8")
+lab1.pack()
+
+result_label = Label(window, text="", font="Times 15", fg="DarkSlateBlue", bg="#D8BFD8")
+result_label.pack()
+
+
+window.mainloop()
